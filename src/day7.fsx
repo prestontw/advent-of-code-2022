a2919258ad9f15d62c2ead6d574b3ab7ecce2da4
Preston TW <preston_tunnell_wilson@brown.edu>
2022-12-07 03:05:22 -0500

    Finish part 1
    With a clearer picture of what I wanted the sizes function to do,
    and a much needed break, I started over with a new function and
    figured out where I should use the sizes. I think I tried this at
    one point in the previous attempt, but one quick fix is passing
    the sizes that we have found along so that we can use them for sibling
    calculations. Another thing that was important was passing the
    initial sizes into `fold`.

--- src/day7.fsx
+++ src/day7.fsx
@@ -18,21 +18,18 @@ and Directory = { Children: Map<string, FileEntries> }
 let newFiles = (Directory { Children = Map [] })
 let newDirectory = { Children = Map [] }
 
-// this isn't doing what I would expect it to do
-let sizes fs =
+let rec directoryAndInnerSizes fs path sizes =
+    let folder (size, innerSizes) name entry =
+        match entry with
+        | File s -> (size + s, innerSizes)
+        | Directory d ->
+            let (innerSize, childrenSizes) =
+                directoryAndInnerSizes d (path + "/" + name) innerSizes
 
-    let rec inner dir (prefix: string) (sizes: Map<string, int>) =
-        let folder (acc) name child =
-            match child with
-            | File size -> (acc + size, sizes)
-            | Directory d ->
-                let newPrefix = (prefix + "/" + name)
-                let size, newSizes = inner d newPrefix sizes
-                (acc, newSizes |> Map.add newPrefix size)
+            (innerSize + size, childrenSizes)
 
-        Map.fold folder (0, (Map [])) dir.Children
-
-    inner fs "/" (Map []) |> snd
+    let (size, innerSizes) = fs.Children |> Map.fold folder (0, sizes)
+    (size, innerSizes |> Map.add path size)
 
 let part1 input =
     let lines = parse input
@@ -60,4 +57,22 @@ let part1 input =
         | [] -> tree, []
 
     let fileSystem, [] = traverse newDirectory (lines |> Seq.toList)
-    fileSystem
+    let _totalSize, dirsAndSizes = directoryAndInnerSizes fileSystem "/" (Map [])
+
+    dirsAndSizes
+    |> Map.toSeq
+    |> Seq.map snd
+    |> Seq.filter (fun size -> size < 100000)
+    |> Seq.sum
+
+let tests =
+    testList
+        "parts"
+        [ test "part 1" {
+              let subject = part1 Day7.data
+              Expect.equal subject 0 ""
+          }
+
+          ]
+
+let main = runTestsWithCLIArgs [] [||] tests
