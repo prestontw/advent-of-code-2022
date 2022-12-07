4b5c98cc51b3f336d1cc5cff6ad391ea10aa179b
Preston TW <preston_tunnell_wilson@brown.edu>
2022-12-07 01:22:55 -0500

    Get the structure of sizes down,
    but get too confused and so take a break. I knew that I could fold
    over the children, but got confused exactly how these should call
    each other or which sizes to use where. I took a break to relax
    and mull this over in the back of my mind.

--- src/day7.fsx
+++ src/day7.fsx
@@ -9,17 +9,36 @@ let parse input =
     let lines = input |> lines
     Seq.map (spaces) lines
 
-type FileSystem =
-    { Children: Map<string, FileSystem>
-      Files: Map<string, int> }
+type FileEntries =
+    | File of int
+    | Directory of Directory
 
-let newFiles = { Children = Map []; Files = Map [] }
+and Directory = { Children: Map<string, FileEntries> }
+
+let newFiles = (Directory { Children = Map [] })
+let newDirectory = { Children = Map [] }
+
+// this isn't doing what I would expect it to do
+let sizes fs =
+
+    let rec inner dir (prefix: string) (sizes: Map<string, int>) =
+        let folder (acc) name child =
+            match child with
+            | File size -> (acc + size, sizes)
+            | Directory d ->
+                let newPrefix = (prefix + "/" + name)
+                let size, newSizes = inner d newPrefix sizes
+                (acc, newSizes |> Map.add newPrefix size)
+
+        Map.fold folder (0, (Map [])) dir.Children
+
+    inner fs "/" (Map []) |> snd
 
 let part1 input =
     let lines = parse input
 
-    let rec traverse (tree: FileSystem) (command: list<string[]>) =
-        let { Children = children; Files = files } = tree
+    let rec traverse (tree: Directory) (command: list<string[]>) =
+        let { Children = children } = tree
 
         match command with
         | current :: rest ->
@@ -28,39 +47,17 @@ let part1 input =
                 match current[2] with
                 | ".." -> tree, rest
                 | child ->
-                    let inner, remaining = traverse newFiles rest
-                    let newDirs = children |> Map.add child inner
-                    traverse { Children = newDirs; Files = files } remaining
+                    let inner, remaining = traverse newDirectory rest
+                    let newDirs = children |> Map.add child (Directory(inner))
+                    traverse { Children = newDirs } remaining
             | "$", "ls" -> traverse tree rest
             | "dir", _entry -> traverse tree rest
             | size, fileName ->
                 let size = int size
-                let newFiles = files |> Map.add fileName size
+                let newFiles = children |> Map.add fileName (File size)
 
-                traverse
-                    { Children = children
-                      Files = newFiles }
-                    rest
+                traverse { Children = newFiles } rest
         | [] -> tree, []
 
-    let fileSystem = traverse newFiles (lines |> Seq.toList)
-    fileSystem |> Map.toSeq |> Seq.length
-
-let tests =
-    testList
-        "parts"
-        [
-
-          test "part 1" {
-              let subject = part1 Day7.data
-              Expect.equal subject 1 ""
-          }
-
-          test "part 2" {
-              let subject = part1 Day7.data
-              Expect.equal subject 1 ""
-          }
-
-          ]
-
-let main = runTestsWithCLIArgs [] [||] tests
+    let fileSystem, [] = traverse newDirectory (lines |> Seq.toList)
+    fileSystem
