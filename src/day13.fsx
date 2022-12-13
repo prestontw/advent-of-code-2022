551528fd9c35622eeb6114f1c105efd7b0cb0b7e
Preston TW <preston_tunnell_wilson@brown.edu>
2022-12-13 02:30:05 -0500

    p1

--- src/day13.fsx
+++ src/day13.fsx
@@ -1,5 +1,5 @@
 #load "Common.fsx"
-#load "../data/day0.fsx"
+#load "../data/day13.fsx"
 #r "nuget: Expecto"
 
 open Common
@@ -13,7 +13,7 @@ type Packet =
     | Int of int
     | PList of Packet list
 
-let parsePacket (line: string) =
+let parseSExpr (line: string) =
     let rec inner (sentence: SExpr list) (pendingWord: string option) (remaining: string) =
         if remaining = "" then
 
@@ -51,29 +51,90 @@ let parsePacket (line: string) =
     // take off the first '[' because we start with a list already
     inner [] None line[1..]
 
+let rec parsePacket sexpr =
+    match sexpr with
+    | Word s -> Int(int s)
+    | Sentence words -> words |> List.map parsePacket |> PList
 
 let parse input =
     let lines = input |> blankLines
+
     lines
+    |> Seq.map (fun pair ->
+        let [| l; r |] = pair |> Array.map (parseSExpr >> fst >> parsePacket)
+        l, r)
+
+let rec lLessThanR lPacket rPacket =
+    match lPacket, rPacket with
+    | Int l, Int r when l < r -> Some true
+    | Int l, Int r when l > r -> Some false
+    | Int _, Int _ -> None
+    | PList(l :: remainingL), PList(r :: remainingR) ->
+        match lLessThanR l r with
+        | None -> lLessThanR (PList remainingL) (PList remainingR)
+        | a -> a
+    | PList [], PList(_ :: _) -> Some true
+    | PList(_ :: _), PList [] -> Some false
+    | PList [], PList [] -> None
+    | PList l, Int r -> lLessThanR (PList l) (PList [ Int r ])
+    | Int l, PList r -> lLessThanR (PList [ Int l ]) (PList r)
 
 
 let part1 input =
-    let lines = parse input
+    let packetPairs = parse input
 
-    1
+    packetPairs
+    |> Seq.indexed
+    |> Seq.choose (fun (i, (l, r)) ->
+        if (lLessThanR l r |> Option.defaultValue false) then
+            Some(i + 1)
+        else
+            None)
+    |> Seq.sum
 
 let tests =
     testList
         "parts"
         [
 
-          //   test "parse" {
-          //       let output = parsePacket "[[],[1,2],3]"
-          //       let expected = List([ List []; List [ Int 1; Int 2 ]; Int 3 ])
-          //       Expect.equal output expected ""
-          //   }
+          test "parse" {
+              let output = "[[],[1,2],3]" |> parseSExpr |> fst |> parsePacket
+              let expected = PList([ PList []; PList [ Int 1; Int 2 ]; Int 3 ])
+              Expect.equal output expected ""
+          }
+
+          test "sample" {
+              let output =
+                  part1
+                      "[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]"
+
+              Expect.equal output 13 ""
+          }
+
           test "part 1" {
-              let subject = part1 Day0.data
+              let subject = part1 Day13.data
               Expect.equal subject 1 ""
           }
 
