f9340484b1d1e1490c801637b7fd92d814ef3604
Preston TW <preston_tunnell_wilson@brown.edu>
2022-12-15 00:53:23 -0500

    Pass sample

--- src/day15.fsx
+++ src/day15.fsx
@@ -30,7 +30,7 @@ type Cell =
     | Sensor
     | NotBeacon
 
-let part1 input =
+let part1a input =
     let lines = parse input
 
     let folder (grid: Map<_, _>) (sensor, beacon) =
@@ -39,9 +39,9 @@ let part1 input =
 
         let positions =
             seq {
-                for x in (sensorX - distance) .. (sensorX + distance) do
-                    for y in (sensorY - distance) .. (sensorY + distance) do
-                        if manhattanPoints (x, y) sensor < distance then
+                for x in (sensorX - distance - 1) .. (sensorX + distance + 1) do
+                    for y in (sensorY - distance - 1) .. (sensorY + distance + 1) do
+                        if manhattanPoints (x, y) sensor <= distance then
                             yield (x, y)
             }
 
@@ -50,8 +50,7 @@ let part1 input =
             |> Seq.fold
                 (fun grid pos ->
                     match grid |> Map.tryFind pos with
-                    | None when (manhattanPoints sensor pos) < distance -> grid |> Map.add pos NotBeacon
-                    | None
+                    | None -> grid |> Map.add pos NotBeacon
                     | Some _ -> grid)
                 grid
 
@@ -60,19 +59,45 @@ let part1 input =
 
     let grid = lines |> Seq.fold folder (Map [])
 
-    grid
-    |> Map.toSeq
+    grid |> Map.toSeq
+
+let part1b p1YCoord s =
+    s
     |> Seq.filter (fun ((_, y), cell) -> y = p1YCoord && cell = NotBeacon)
     |> Seq.length
+
+let part1 input ycoord = part1a input |> part1b ycoord
+
+let sample =
+    "Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3"
 
 let tests =
     testList
         "parts"
         [
 
-          test "part 1" {
-              let subject = part1 Day15.data
-              Expect.equal subject 1 ""
+          //   test "part 1" {
+          //       let subject = part1 Day15.data p1YCoord
+          //       Expect.equal subject 1 ""
+          //   }
+
+          test "sample" {
+              let subject = part1 sample 10
+
+              Expect.equal subject 26 ""
           }
 
           ]
