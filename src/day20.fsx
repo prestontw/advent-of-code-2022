63b45ac04973bb60626d418e6f3aa6456453dfa3
Preston TW <preston_tunnell_wilson@brown.edu>
2022-12-20 10:39:39 -0600

    p1

--- src/day20.fsx
+++ src/day20.fsx
@@ -7,7 +7,7 @@ open Expecto
 
 let parse input =
     let lines = input |> lines
-    Seq.map (int) lines
+    Seq.map (int) lines |> Seq.indexed
 
 let moddedIndex l i =
     let len = List.length l
@@ -23,23 +23,26 @@ let groveCoordinates l =
 
 let insertAtSpaces l num =
     let index = l |> List.findIndex ((=) num)
+    let originalIndex, num = num
     let length = l |> List.length
     let newIndex = index + (num % (length - 1))
 
     if newIndex >= length then
         let untilEnd = (length - 1) - index
         let newIndex = (num - untilEnd) % (length - 1)
-        l |> List.removeAt index |> List.insertAt (newIndex) num
+        l |> List.removeAt index |> List.insertAt (newIndex) (originalIndex, num)
     else if newIndex < 0 then
-        l |> List.removeAt index |> List.insertAt ((newIndex - 1 + length) % length) num
+        l
+        |> List.removeAt index
+        |> List.insertAt ((newIndex - 1 + length) % length) (originalIndex, num)
     else
-        l |> List.removeAt index |> List.insertAt newIndex num
+        l |> List.removeAt index |> List.insertAt newIndex (originalIndex, num)
 
 let normalize l =
     let rec inner l pending =
         match l with
         | [] -> failwith "expected to find 0"
-        | h :: _ when h = 0 -> List.append l (List.rev pending)
+        | (_, h) :: _ when h = 0 -> List.append l (List.rev pending)
         | h :: rest -> inner rest (h :: pending)
 
     inner l []
@@ -48,13 +51,7 @@ let tee = id
 
 let mix originalNums =
     let afterOne =
-        originalNums
-        |> List.fold
-            (fun acc num ->
-                let index = tee ((acc) |> List.findIndex ((=) (tee num)))
-
-                (insertAtSpaces acc num))
-            originalNums
+        originalNums |> List.fold (fun acc num -> (insertAtSpaces acc num)) originalNums
 
     afterOne
 
@@ -63,7 +60,7 @@ let part1 input =
     let nums = parse input
     let originalNums = nums |> Seq.toList
 
-    originalNums |> mix |> groveCoordinates
+    originalNums |> mix |> List.map snd |> groveCoordinates
 
 let tests =
     testList
@@ -75,21 +72,27 @@ let tests =
               Expect.equal subject 3 ""
           }
           test "around times" {
-              let subject = (parse >> Seq.toList >> mix) "1\n2\n10\n-3\n3\n-2\n-11\n0\n4"
+              let subject =
+                  (parse >> Seq.toList >> mix >> List.map snd) "1\n2\n10\n-3\n3\n-2\n-11\n0\n4"
+
               Expect.equal subject [ 1; -11; -2; 2; 4; 10; 0; 3; -3 ] ""
           }
           test "manageable multiple times" {
-              let subject = (parse >> Seq.toList >> mix >> normalize) "20\n-22\n4\n0\n5"
+              let subject =
+                  (parse >> Seq.toList >> mix >> normalize >> List.map snd) "20\n-22\n4\n0\n5"
+
               Expect.equal subject [ 0; -22; 20; 5; 4 ] ""
           }
           test "manageable multiple times 2" {
-              let subject = (parse >> Seq.toList >> mix >> normalize) "21\n-22\n-4\n0\n5"
+              let subject =
+                  (parse >> Seq.toList >> mix >> normalize >> List.map snd) "21\n-22\n-4\n0\n5"
+
               Expect.equal subject [ 0; 21; 5; -4; -22 ] ""
           }
 
           test "part 1" {
               let subject = part1 Day20.data
-              Expect.equal subject 1 ""
+              Expect.equal subject 4267 ""
               Expect.notEqual subject -20632 ""
           }
 
