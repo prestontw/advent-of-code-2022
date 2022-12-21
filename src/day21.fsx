c312e7b6f8cc47de23fb4c7e08977c9ac3df45cb
Preston TW <preston_tunnell_wilson@brown.edu>
2022-12-20 23:48:13 -0600

    first draft p2

--- src/day21.fsx
+++ src/day21.fsx
@@ -9,6 +9,11 @@ type Monkey =
     | Plain of int64
     | Composite of char * string * string
 
+type Expression =
+    | Numeric of int64
+    | Deferred of char * Expression * Expression
+    | Human
+
 let plainRegex = "(.*): (\d+)"
 let compositeRegex = "(.*): (.+) (.) (.+)"
 
@@ -39,24 +44,32 @@ let part1 input =
         | None ->
             let results, result =
                 match monkeys[monkey] with
-                | Plain d -> results, d
+                | Plain d -> results, Numeric d
                 | Composite(op, child1, child2) ->
                     let results, child1Result = eval results child1
                     let results, child2Result = eval results child2
 
                     let result =
-                        match op with
-                        | '+' -> child1Result + child2Result
-                        | '-' -> child1Result - child2Result
-                        | '*' -> child1Result * child2Result
-                        | '/' -> child1Result / child2Result
-                        | c -> failwithf "unknown op %A" c
+                        match op, child1Result, child2Result with
+                        | '+', Numeric child1Result, Numeric child2Result -> Numeric(child1Result + child2Result)
+                        | '+', a, b -> Deferred(op, a, b)
+                        | '-', Numeric child1Result, Numeric child2Result -> Numeric(child1Result - child2Result)
+                        | '-', c1, c2 when c1 = c2 -> Numeric 0
+                        | '-', a, b -> Deferred(op, a, b)
+                        | '*', Numeric child1Result, Numeric child2Result -> Numeric(child1Result * child2Result)
+                        | '*', Numeric 0L, _
+                        | '*', _, Numeric 0L -> Numeric 0
+                        | '*', a, b -> Deferred(op, a, b)
+                        | '/', Numeric child1Result, Numeric child2Result -> Numeric(child1Result / child2Result)
+                        | '/', Numeric 0L, _ -> Numeric 0
+                        | '/', a, b -> Deferred(op, a, b)
+                        | c, _, _ -> failwithf "unknown op %A" c
 
                     results, result
 
             (results |> Map.add monkey result, result)
 
-    eval (Map []) "root" |> snd
+    eval (Map [ ("humn", Human) ]) "root" |> snd
 
 let tests =
     testList
@@ -65,11 +78,11 @@ let tests =
 
           test "sample" {
               let subject = part1 Day21.sample
-              Expect.equal subject 152 ""
+              Expect.equal subject (Numeric 1) ""
           }
           test "part 1" {
               let subject = part1 Day21.data
-              Expect.equal subject 1 ""
+              Expect.equal subject (Numeric 1) ""
           }
 
           ]
