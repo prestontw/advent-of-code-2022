4ac5fcb804bfcefb64f18916367d3c27f20deaac
Preston TW <preston_tunnell_wilson@brown.edu>
2022-12-24 04:18:07 -0600

    Get sample passing

--- src/day24.fsx
+++ src/day24.fsx
@@ -45,21 +45,25 @@ let charToCell c =
 let parse input =
     let lines = input |> lines
 
-    let accLine line rowNum =
+    let accLine line y =
         line
         |> Seq.indexed
-        |> Seq.fold (fun acc (i, c) -> acc |> Map.add (rowNum, i) (charToCell c)) (Map [])
+        |> Seq.fold (fun acc (i, c) -> acc |> Map.add (i, y) (charToCell c)) (Map [])
 
     lines
     |> Seq.indexed
-    |> Seq.fold (fun acc (rowNum, line) -> acc |> Map.union (accLine line rowNum)) (Map [])
+    |> Seq.fold (fun acc (y, line) -> acc |> Map.union (accLine line y)) (Map [])
 
 
-let gridWidth grid =
+let maxY grid =
+    grid |> Map.toSeq |> Seq.map (fst >> snd) |> Seq.max
+
+let maxX grid =
     grid |> Map.toSeq |> Seq.map (fst >> fst) |> Seq.max
 
-let gridHeight grid =
-    grid |> Map.toSeq |> Seq.map (fst >> snd) |> Seq.max
+let isRaging blizzard =
+    let sum = blizzard.Upwards + blizzard.Downwards + blizzard.Left + blizzard.Right
+    sum <> 0
 
 let getOrWrap direct wrap grid =
     let directCell = grid |> Map.find direct
@@ -74,6 +78,7 @@ let getOrWrap direct wrap grid =
 
 let directionOrWrap pos direction (grid, maxWidth, maxHeight) =
     let (x, y) = pos
+
 
     match direction with
     | Left -> getOrWrap (x - 1, y) (maxWidth - 1, y) grid
@@ -93,89 +98,135 @@ let wrappedWalledNeighbors pos gridInfo =
        Up = up
        Down = down |}
 
-let simulate grid maxWidth maxHeight =
+let simulate grid maxX maxY =
     let starting =
         grid
         |> Map.toSeq
         |> Seq.map (fun (pos, cell) ->
             match cell with
             | Wall -> (pos, Wall)
-            | _ -> (pos, Blizzard emptyBlizzard))
+            | Expedition -> (pos, Expedition)
+            | Sink -> (pos, Sink)
+            | Blizzard _ -> (pos, Blizzard emptyBlizzard))
         |> Map.ofSeq
 
-    let leftCount counts =
-        match counts with
-        | Blizzard { Left = u } -> u
-        | _ -> 0
-
-    let rightCount counts =
-        match counts with
-        | Blizzard { Right = u } -> u
-        | _ -> 0
-
-    let upcount counts =
-        match counts with
-        | Blizzard { Upwards = u } -> u
-        | _ -> 0
-
-    let downCount counts =
-        match counts with
-        | Blizzard { Downwards = u } -> u
-        | _ -> 0
-
-    let unwrapBlizzard cell =
-        match cell with
-        | Blizzard b -> b
 
     grid
     |> Map.toSeq
     |> Seq.fold
         (fun (acc: Map<(_ * _), _>) (pos, cell) ->
-            tee pos |> ignore
 
             match cell with
             | Blizzard { Upwards = u
                          Downwards = d
                          Left = l
                          Right = r } ->
-                let neighbors = wrappedWalledNeighbors pos (acc, maxWidth, maxHeight)
+                let neighbors = wrappedWalledNeighbors pos (acc, maxX, maxY)
 
 
                 let acc =
                     let (k1, k2, receiver) = neighbors.Left
-                    let receiver = receiver |> unwrapBlizzard
-                    acc |> Map.add (k1, k2) (Blizzard { receiver with Left = l + receiver.Left })
+
+                    match receiver with
+                    | Blizzard receiver -> acc |> Map.add (k1, k2) (Blizzard { receiver with Left = l + receiver.Left })
+                    | _ -> acc
 
                 let acc =
                     let (k1, k2, receiver) = neighbors.Right
-                    let receiver = receiver |> unwrapBlizzard
-                    acc |> Map.add (k1, k2) (Blizzard { receiver with Right = r + receiver.Right })
+
+                    match receiver with
+                    | Blizzard receiver ->
+                        acc |> Map.add (k1, k2) (Blizzard { receiver with Right = r + receiver.Right })
+                    | _ -> acc
 
                 let acc =
                     let (k1, k2, receiver) = neighbors.Up
-                    let receiver = receiver |> unwrapBlizzard
 
-                    acc
-                    |> Map.add (k1, k2) (Blizzard { receiver with Upwards = u + receiver.Upwards })
+                    match receiver with
+                    | Blizzard receiver ->
+                        acc
+                        |> Map.add (k1, k2) (Blizzard { receiver with Upwards = u + receiver.Upwards })
+                    | _ -> acc
 
                 let acc =
                     let (k1, k2, receiver) = neighbors.Down
-                    let receiver = receiver |> unwrapBlizzard
 
-                    acc
-                    |> Map.add (k1, k2) (Blizzard { receiver with Downwards = d + receiver.Downwards })
+                    match receiver with
+                    | Blizzard receiver ->
+                        acc
+                        |> Map.add (k1, k2) (Blizzard { receiver with Downwards = d + receiver.Downwards })
+                    | _ -> acc
 
                 acc
             | _ -> acc)
         starting
 
+// keep track of different boards
+let aStar (grid, maxWidth, maxHeight) (starts: seq<(int * int)>) dest =
+    let openSet = System.Collections.Generic.PriorityQueue()
+    starts |> Seq.iter (fun t -> openSet.Enqueue((0, t, grid), 0))
+    let costs = Map(starts |> Seq.map (fun pos -> (0, pos), 0))
+
+    let estimate (x, y) time =
+        let destX, destY = dest
+        (absDiff x destX) + (absDiff y destY) + time
+
+    let rec next (gScore: Map<(int * (int * int)), int>) =
+        let folder time prevPos nowPos cost nextBoard acc =
+            let newScore = (acc |> Map.find (time, prevPos)) + cost
+            let potential = acc |> Map.tryFind (time + 1, nowPos)
+            let estimate = estimate nowPos time
+
+            match potential with
+            | Some c when c > newScore ->
+                let ret = acc |> Map.add ((time + 1), nowPos) newScore
+                openSet.Enqueue((time + 1, nowPos, nextBoard), newScore + estimate)
+                ret
+            | Some _ -> acc
+            | None ->
+                let ret = acc |> Map.add (time + 1, nowPos) newScore
+                openSet.Enqueue((time + 1, nowPos, nextBoard), newScore + estimate)
+                ret
+
+        if (openSet.Count) = 0 then
+            None
+        else
+            let (time, pos, board) = openSet.Dequeue()
+            printf "%A %A\n" time pos
+            let nextBoard = simulate board maxWidth maxHeight
+
+            let reachable p =
+                nextBoard
+                |> Map.tryFind p
+                |> fun cell ->
+                    match cell with
+                    | None -> false
+                    | Some(Blizzard b) when not (b |> isRaging) -> true
+                    | Some(Blizzard _)
+                    | Some Wall -> false
+                    | _ -> true
+
+            if pos = dest then
+                gScore |> Map.tryFind (time, pos)
+            else
+                let nexts = pos |> cardinalNeighbors |> List.append [ pos ] |> Seq.filter reachable
+
+                next (nexts |> Seq.fold (fun acc cur -> folder time pos cur 1 nextBoard acc) gScore)
+
+    next costs
+
+
+
 let part1 input =
     let lines = parse input
 
-    let maxWidth = lines |> gridWidth
-    let maxHeight = lines |> gridHeight
+    let maxX = lines |> maxX
+    let maxY = lines |> maxY
 
-    1
+    let start = lines |> Map.toSeq |> Seq.find (fun s -> snd s = Expedition) |> fst
+    let dest = lines |> Map.toSeq |> Seq.find (fun s -> snd s = Sink) |> fst
+
+    aStar (lines, maxX, maxY) [ start ] dest
 
 let tests =
     testList
@@ -184,12 +235,12 @@ let tests =
 
           test "sample" {
               let subject = part1 Day24.sample
-              Expect.equal subject 18 ""
+              Expect.equal subject (Some 18) ""
           }
-          test "part 1" {
-              let subject = part1 Day24.data
-              Expect.equal subject 1 ""
-          }
+          //   test "part 1" {
+          //       let subject = part1 Day24.data
+          //       Expect.equal subject None ""
+          //   }
 
           ]
 
