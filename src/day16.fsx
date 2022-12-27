dc0260c4d0b0fb34a9760bac9642ae5b0ed46943
Preston TW <preston_tunnell_wilson@brown.edu>
2022-12-27 10:36:22 -0600

    first draft

--- src/day16.fsx
+++ src/day16.fsx
@@ -5,15 +5,85 @@
 open Common
 open Expecto
 
+let lineRegex = "Valve (.*) has flow rate=(\d+); tunnels? leads? to valves? (.*)"
+let matchLine = extractValues lineRegex
+
+type Valve =
+    { name: string
+      flowRate: int
+      neighbors: string array
+      isOpen: bool }
+
 let parse input =
     let lines = input |> lines
-    Seq.map (spaces) lines
+
+    let parseLine line =
+        let [ valveName; flowRate; neighbors ] = matchLine line |> Option.get |> Seq.toList
+        let flowRate = int flowRate
+        let neighbors = neighbors.Split ", "
+
+        { name = valveName
+          flowRate = flowRate
+          neighbors = neighbors
+          isOpen = false }
+
+    Seq.map parseLine lines
+
+let mapByName valves =
+    valves |> Seq.map (fun v -> (v.name, v)) |> Map.ofSeq
+
+let bfs valves start =
+    let rec inner queue seen =
+        if queue |> Seq.isEmpty then
+            seen
+        else
+            let (valve, cost) = queue |> Seq.head
+
+            let (queue, seen) =
+                valve.neighbors
+                |> Seq.fold
+                    (fun (queue, seen) cur ->
+                        let cur = valves |> Map.find cur
+
+                        if seen |> Map.containsKey cur then
+                            (queue, seen)
+                        else
+                            (Seq.append queue [ cur, cost + 1 ]), (seen |> Map.add cur (cost + 1)))
+                    ((queue |> Seq.tail), seen)
+
+            inner queue seen
+
+    inner (seq { start, 0 }) (Map [ start, 0 ])
+
+let openingCost = 1
+
+let calculate valves start timeLength =
+    let rec inner location time totalFlow valves =
+        let costsToOthers = bfs valves location
+
+        let (nextLocation, flowAdded) =
+            costsToOthers
+            |> Map.toSeq
+            |> Seq.filter (fun (v, _) -> not v.isOpen)
+            |> Seq.map (fun (valve, stepsThere) ->
+                valve, valve.flowRate * (timeLength - (time + stepsThere + openingCost)))
+            |> Seq.sortDescending
+            |> Seq.head
+
+        inner
+            nextLocation
+            (time + (costsToOthers |> Map.find nextLocation) + openingCost)
+            (totalFlow + flowAdded)
+            (valves |> Map.add nextLocation.name { nextLocation with isOpen = true })
+
+    inner (valves |> Map.find start) 0 0 valves
 
 
 let part1 input =
     let lines = parse input
+    let valves = mapByName lines
 
-    1
+    calculate valves "AA" 30
 
 let tests =
     testList
@@ -23,6 +93,11 @@ let tests =
           test "part 1" {
               let subject = part1 Day16.data
               Expect.equal subject 1 ""
+          }
+
+          test "sample" {
+              let subject = part1 Day16.sample
+              Expect.equal subject 1651 ""
           }
 
           ]
