0fac2c3cd2f18742fb844e89189db30001818a4d
Preston TW <preston_tunnell_wilson@brown.edu>
2023-01-02 03:15:43 -0600

    Separate out into p1 and p2

--- src/day3.fsx
+++ src/day3.fsx
@@ -9,8 +9,31 @@ let parse input =
     let lines = input |> lines
     lines
 
+let scoreDuplicates duplicate items =
+    items
+    |> Seq.map duplicate
+    |> Seq.sumBy (fun duplicate ->
+        if duplicate > 'Z' then
+            int duplicate - (int 'a') + 1
+        else
+            int duplicate - (int 'A') + 27)
 
 let part1 input =
+    let lines = parse input
+
+    let duplicate (line: string) =
+        let half = (line.Length - 1) / 2
+        let first, second = line[0..half], line[half + 1 .. line.Length]
+
+        first
+        |> Set.ofSeq
+        |> Set.intersect (second |> Set.ofSeq)
+        |> Set.toList
+        |> List.head
+
+    lines |> scoreDuplicates duplicate
+
+let part2 input =
     let lines = parse input
 
     let duplicate (lines: string[]) =
@@ -21,14 +44,7 @@ let part1 input =
         |> fst
 
 
-    lines
-    |> Seq.chunkBySize 3
-    |> Seq.map duplicate
-    |> Seq.sumBy (fun duplicate ->
-        if duplicate > 'Z' then
-            int duplicate - (int 'a') + 1
-        else
-            int duplicate - (int 'A') + 27)
+    lines |> Seq.chunkBySize 3 |> scoreDuplicates duplicate
 
 let tests =
     testList
@@ -37,12 +53,12 @@ let tests =
 
           test "part 1" {
               let subject = part1 Day3.data
-              Expect.equal subject 1 ""
+              Expect.equal subject 8072 ""
           }
 
           test "part 2" {
-              let subject = part1 Day3.data
-              Expect.equal subject 1 ""
+              let subject = part2 Day3.data
+              Expect.equal subject 2567 ""
           }
 
           ]
